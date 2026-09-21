04b8d92d1c1de7542e25a435ecc8e28151e25b89
Mohamed Halim <mohamed.abdelhalim@vodafone.com>
2026-09-21 14:41:05 +0300

    feat: implement RESP encoder

---
+++ mini-redis/src/main/java/com/mohamed/halim/miniredis/RespEncoderImpl.java
@@ -0,0 +1,67 @@
+package com.mohamed.halim.miniredis;
+
+import com.mohamed.halim.miniredis.resp.RespType;
+
+import java.util.List;
+
+public class RespEncoderImpl implements RespEncoder{
+    public static final String END_LINE = "\r\n";
+    @Override
+    public byte[] encodeSimpleString(String value) {
+        String builder = RespType.SIMPLE.getPrefix() +
+                value +
+                END_LINE;
+        return builder.getBytes();
+    }
+
+    @Override
+    public byte[] encodeError(String errorType, String message) {
+        String builder = RespType.ERROR.getPrefix() +
+                errorType + " " + message +
+                END_LINE;
+        return builder.getBytes();
+    }
+
+    @Override
+    public byte[] encodeInteger(long value) {
+        String builder = RespType.INTEGER.getPrefix() +
+                String.valueOf(value) +
+                END_LINE;
+        return builder.getBytes();
+    }
+
+    @Override
+    public byte[] encodeBulkString(String value) {
+        if(value == null) {
+            return "$-1\r\n".getBytes();
+        }
+        String builder = encodeString(value);
+        return builder.getBytes();
+    }
+
+    private String encodeString(String value) {
+        return (String.valueOf(RespType.BULK_STRING.getPrefix()))
+                .concat(String.valueOf(value.length()))
+                .concat(END_LINE)
+                .concat(value)
+                .concat(END_LINE);
+    }
+
+    @Override
+    public byte[] encodeArray(List<String> elements) {
+        if(elements == null) {
+            return "*-1\r\n".getBytes();
+        }
+        StringBuilder builder = new StringBuilder();
+        builder.append(RespType.ARRAY.getPrefix())
+                .append(elements.size())
+                .append(END_LINE);
+       elements.stream().map(this::encodeString).forEach(builder::append);
+       return builder.toString().getBytes();
+    }
+
+    @Override
+    public byte[] encodePubSubMessage(String channel, String message) {
+        return new byte[0];
+    }
+}
--- mini-redis/src/test/java/phase1/RespEncoderTest.java
+++ mini-redis/src/test/java/phase1/RespEncoderTest.java
@@ -16,13 +16,12 @@ import static org.assertj.core.api.Assertions.*;
 @DisplayName("RESP Encoder")
 class RespEncoderTest {
 
-    // TODO: Replace with your implementation
-    // private RespEncoder encoder;
+     private RespEncoder encoder;
 
-    // @BeforeEach
-    // void setUp() {
-    //     encoder = new MiniRedisRespEncoder();
-    // }
+     @BeforeEach
+     void setUp() {
+         encoder = new RespEncoderImpl();
+     }
 
     private String decode(byte[] bytes) {
         return new String(bytes, StandardCharsets.UTF_8);
@@ -31,79 +30,79 @@ class RespEncoderTest {
     @Test
     @DisplayName("should encode simple string")
     void shouldEncodeSimpleString() {
-        // assertThat(decode(encoder.encodeSimpleString("OK"))).isEqualTo("+OK\r\n");
-        fail("Implement RespEncoder and uncomment this test");
+      assertThat(decode(encoder.encodeSimpleString("OK"))).isEqualTo("+OK\r\n");
+        
     }
 
     @Test
     @DisplayName("should encode PONG")
     void shouldEncodePong() {
-        // assertThat(decode(encoder.encodeSimpleString("PONG"))).isEqualTo("+PONG\r\n");
-        fail("Implement RespEncoder and uncomment this test");
+      assertThat(decode(encoder.encodeSimpleString("PONG"))).isEqualTo("+PONG\r\n");
+        
     }
 
     @Test
     @DisplayName("should encode error")
     void shouldEncodeError() {
-        // assertThat(decode(encoder.encodeError("ERR", "unknown command 'foo'")))
-        //         .isEqualTo("-ERR unknown command 'foo'\r\n");
-        fail("Implement RespEncoder and uncomment this test");
+      assertThat(decode(encoder.encodeError("ERR", "unknown command 'foo'")))
+               .isEqualTo("-ERR unknown command 'foo'\r\n");
+        
     }
 
     @Test
     @DisplayName("should encode integer")
     void shouldEncodeInteger() {
-        // assertThat(decode(encoder.encodeInteger(42))).isEqualTo(":42\r\n");
-        fail("Implement RespEncoder and uncomment this test");
+      assertThat(decode(encoder.encodeInteger(42))).isEqualTo(":42\r\n");
+        
     }
 
     @Test
     @DisplayName("should encode negative integer")
     void shouldEncodeNegativeInteger() {
-        // assertThat(decode(encoder.encodeInteger(-1))).isEqualTo(":-1\r\n");
-        fail("Implement RespEncoder and uncomment this test");
+      assertThat(decode(encoder.encodeInteger(-1))).isEqualTo(":-1\r\n");
+        
     }
 
     @Test
     @DisplayName("should encode bulk string")
     void shouldEncodeBulkString() {
-        // assertThat(decode(encoder.encodeBulkString("hello"))).isEqualTo("$5\r\nhello\r\n");
-        fail("Implement RespEncoder and uncomment this test");
+      assertThat(decode(encoder.encodeBulkString("hello"))).isEqualTo("$5\r\nhello\r\n");
+        
     }
 
     @Test
     @DisplayName("should encode null bulk string")
     void shouldEncodeNullBulkString() {
-        // assertThat(decode(encoder.encodeBulkString(null))).isEqualTo("$-1\r\n");
-        fail("Implement RespEncoder and uncomment this test");
+      assertThat(decode(encoder.encodeBulkString(null))).isEqualTo("$-1\r\n");
+        
     }
 
     @Test
     @DisplayName("should encode empty bulk string")
     void shouldEncodeEmptyBulkString() {
-        // assertThat(decode(encoder.encodeBulkString(""))).isEqualTo("$0\r\n\r\n");
-        fail("Implement RespEncoder and uncomment this test");
+      assertThat(decode(encoder.encodeBulkString(""))).isEqualTo("$0\r\n\r\n");
+        
     }
 
     @Test
     @DisplayName("should encode array of bulk strings")
     void shouldEncodeArray() {
-        // assertThat(decode(encoder.encodeArray(List.of("foo", "bar"))))
-        //         .isEqualTo("*2\r\n$3\r\nfoo\r\n$3\r\nbar\r\n");
-        fail("Implement RespEncoder and uncomment this test");
+      assertThat(decode(encoder.encodeArray(List.of("foo", "bar"))))
+                 .isEqualTo("*2\r\n$3\r\nfoo\r\n$3\r\nbar\r\n");
+        
     }
 
     @Test
     @DisplayName("should encode null array")
     void shouldEncodeNullArray() {
-        // assertThat(decode(encoder.encodeArray(null))).isEqualTo("*-1\r\n");
-        fail("Implement RespEncoder and uncomment this test");
+      assertThat(decode(encoder.encodeArray(null))).isEqualTo("*-1\r\n");
+        
     }
 
     @Test
     @DisplayName("should encode empty array")
     void shouldEncodeEmptyArray() {
-        // assertThat(decode(encoder.encodeArray(List.of()))).isEqualTo("*0\r\n");
-        fail("Implement RespEncoder and uncomment this test");
+      assertThat(decode(encoder.encodeArray(List.of()))).isEqualTo("*0\r\n");
+        
     }
 }
